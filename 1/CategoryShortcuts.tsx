be113fe2373ea52a84010f19042410f7ba379ebc
baobao127 <bechauonmyojituusp@gmail.com>
2025-05-17 09:59:13 +0700

    Delete 1 directory

--- 1/CategoryShortcuts.tsx
+++
@@ -1,9 +0,0 @@
-const categories: CategoryItem[] = [
-  {
-    name: "Áo",
-    slug: "ao",
-    icon: "fa-tshirt", // Kiểm tra icon tồn tại
-    bgColor: "bg-primary/10",
-    textColor: "text-primary"
-  },
-];
